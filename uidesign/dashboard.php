7e1693dbeb57b22330b32380a0a63bce88c0b5cc
fahri firdausillah <elfaatta@gmail.com>
2016-06-24 13:04:36 +0700

    dashboard dipisah

---
+++ uidesign/dashboard.php
@@ -0,0 +1,111 @@
+<div class="page_content">
+  <div class="page-head">
+    <!-- BEGIN PAGE TITLE -->
+    <div class="page-title">
+        <h1>Dashboard
+            <small>dashboard &amp; statistics</small>
+        </h1>
+    </div>
+  </div>
+  <div class="clearfix">&nbsp;</div>
+  <div class="row">
+    <div class="col-md-8">
+        <!-- BEGIN Portlet PORTLET-->
+        <div class="portlet box blue">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="fa fa-gift"></i>Basis Data</div>
+                <ul class="nav nav-tabs">
+                    <li class="active">
+                        <a aria-expanded="true" href="#bd_tab3" data-toggle="tab"> Posting </a>
+                    </li>
+                    <li class="">
+                        <a aria-expanded="false" href="#bd_tab2" data-toggle="tab"> Materi </a>
+                    </li>
+                    <li class="">
+                        <a aria-expanded="false" href="#bd_tab1" data-toggle="tab"> Tugas </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="portlet-body">
+                <div class="tab-content">
+                    <div class="tab-pane" id="bd_tab1">
+                        <p> Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum
+                            zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.ut
+                            laoreet dolore magna ut laoreet dolore magna. ut laoreet dolore magna. ut laoreet dolore magna. </p>
+                    </div>
+                    <div class="tab-pane" id="bd_tab2">
+                        <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                            aliquyam erat, sed diam voluptua. At vero eos et accusam et justo. </p>
+                    </div>
+                    <div class="tab-pane active" id="bd_tab3">
+                        <p> Ut wisi enim ad btn-smm veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+                            consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="portlet box blue">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="fa fa-gift"></i>Algoritma Pemrograman</div>
+                <ul class="nav nav-tabs">
+                    <li class="active">
+                        <a aria-expanded="true" href="#ap_tab3" data-toggle="tab"> Posting </a>
+                    </li>
+                    <li class="">
+                        <a aria-expanded="false" href="#ap_tab2" data-toggle="tab"> Materi </a>
+                    </li>
+                    <li class="">
+                        <a aria-expanded="false" href="#ap_tab1" data-toggle="tab"> Tugas </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="portlet-body">
+                <div class="tab-content">
+                    <div class="tab-pane" id="ap_tab1">
+                        <p> Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum
+                            zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.ut
+                            laoreet dolore magna ut laoreet dolore magna. ut laoreet dolore magna. ut laoreet dolore magna. </p>
+                    </div>
+                    <div class="tab-pane" id="ap_tab2">
+                        <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                            aliquyam erat, sed diam voluptua. At vero eos et accusam et justo. </p>
+                    </div>
+                    <div class="tab-pane active" id="ap_tab3">
+                        <p> Ut wisi enim ad btn-smm veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+                            consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+      <div class="portlet box red">
+        <div class="portlet-title">
+          <div class="caption">
+              <i class="fa fa-gift"></i>Portlet2 </div>
+          <div class="tools">
+              <a title="" data-original-title="" href="javascript:;" class="collapse"> </a>
+              <a title="" data-original-title="" href="" class="fullscreen"> </a>
+              <a title="" data-original-title="" href="#portlet-config" data-toggle="modal" class="config"> </a>
+          </div>
+          <div class="actions">
+              <a href="javascript:;" class="btn btn-default btn-sm">
+                  <i class="fa fa-pencil"></i> Edit </a>
+          </div>
+        </div>
+        <div class="portlet-body">
+          <div style="position: relative; overflow: hidden; width: auto; height: 200px;" class="slimScrollDiv"><div data-initialized="1" class="scroller" style="height: 200px; overflow: hidden; width: auto;" data-always-visible="1" data-rail-visible="1" data-rail-color="blue" data-handle-color="red">
+              <strong>Scroll is always visible</strong>
+              <br> Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula,
+              eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit
+              amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. </div><div style="background: red none repeat scroll 0% 0%; width: 7px; position: absolute; top: 0px; opacity: 0.4; display: block; border-radius: 7px; z-index: 99; right: 1px; height: 117.647px;" class="slimScrollBar"></div><div style="width: 7px; height: 100%; position: absolute; top: 0px; display: block; border-radius: 7px; background: blue none repeat scroll 0% 0%; opacity: 0.2; z-index: 90; right: 1px;" class="slimScrollRail"></div></div>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>
